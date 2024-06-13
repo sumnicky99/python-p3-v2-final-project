ab7d2cfc8a37119dbc8c287f2524a3763b445f43
Sumnicky99 <sumnicky99@gmail.com>
2024-06-13 11:59:44 +0300

    creating a class method for the user

--- lib/user.py
+++ lib/user.py
@@ -1,3 +1,9 @@
 #importing get_db_connection from connection file
 from connection import get_db_connection
 
+#creating a class method for the user
+class User:
+    def __init__(self, username, email, user_id=None):
+        self._username = username
+        self._email = email
+        self._id = user_id
