c204ace0a85e35a499fdb4e92e04b1464c37102f
Sumnicky99 <sumnicky99@gmail.com>
2024-06-13 12:24:52 +0300

    setting the id

--- lib/category.py
+++ lib/category.py
@@ -20,4 +20,10 @@ class Category:
     @property
     def id(self):
         return self._id
+#setting the id
+    @id.setter
+    def id(self, value):
+        if not isinstance(value, int) and value is not None:
+            raise TypeError("ID must be an integer or None.")
+        self._id = value
 
