b30964e4999530bff3ef668d9d37ed0c5a6d2cf5
Sumnicky99 <sumnicky99@gmail.com>
2024-06-13 15:00:47 +0300

    dropping the table

--- initialize_db.py
+++ initialize_db.py
@@ -2,3 +2,10 @@ from lib.user import User
 from lib.category import Category
 from lib.expense import Expense
 from  db.connection  import get_db_connection
+
+# Drop tables
+print("Dropping tables...")
+User.drop_table()
+Category.drop_table()
+Expense.drop_table()
+
