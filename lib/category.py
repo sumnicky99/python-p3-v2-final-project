6bcac8afca8ae7484d2d96a4b8830764c25384e1
Sumnicky99 <sumnicky99@gmail.com>
2024-06-13 12:22:46 +0300

    intializing the class method

--- lib/category.py
+++ lib/category.py
@@ -2,4 +2,8 @@
 from connection import get_db_connection
 #creating the class for category
 class Category:
-   
+    #intializing the class method
+    def __init__(self, name, category_id=None):
+        self._name = name
+        self._id = category_id
+
