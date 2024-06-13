f869ae52659034cc7a2b17d9ed837eaf5d556a38
Sumnicky99 <sumnicky99@gmail.com>
2024-06-13 15:02:04 +0300

    create table and save

--- initialize_db.py
+++ initialize_db.py
@@ -14,6 +14,16 @@ print("Creating tables...")
 User.create_table()
 Category.create_table()
 Expense.create_table()
-created: {user3.username}, {user3.email}, {user3.id}")
+
+# Create and save new users
+print("Creating users...")
+user1 = User.create(username="Maya", email="Maya@example.com")
+print(f"User created: {user1.username}, {user1.email}, {user1.id}")
+
+user2 = User.create(username="John", email="John@example.com")
+print(f"User created: {user2.username}, {user2.email}, {user2.id}")
+
+user3 = User.create(username="Debra", email="Debra@example.com")
+print(f"User created: {user3.username}, {user3.email}, {user3.id}")
 
 
