53aa6fe69890ec9bb32e6d448ec36e3c7d781ce1
Sumnicky99 <sumnicky99@gmail.com>
2024-06-13 12:00:28 +0300

    #adding a property to the username

--- lib/user.py
+++ lib/user.py
@@ -7,3 +7,14 @@ class User:
         self._username = username
         self._email = email
         self._id = user_id
+#adding a property to the username
+    @property
+    def username(self):
+        return self._username
+#setting the username
+    @username.setter
+    def username(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Username must be a string.")
+        self._username = value
+#
